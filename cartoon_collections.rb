f86f538ea39a243b977e22b371be1c9e3856832f
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-07 22:44:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,15 @@ call.capitalize + "!"
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  answer = false 
+planteer_calls.each do |call|
+  if call.length > 4 
+    answer = true 
+  end 
 end
-
+answer 
+end 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
